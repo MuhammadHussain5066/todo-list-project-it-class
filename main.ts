3b5c4fd4c774a6edae4d9796046e053e262abbd7
MuhammadHussain5066 <160261423+MuhammadHussain5066@users.noreply.github.com>
2024-04-26 00:28:11 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+import inquirer from "inquirer";
+let todos : string[]=[]
+let scenerio=true
+while(scenerio){
+let task= await inquirer.prompt([
+    {
+        name: "todo",
+        type: "input",
+        message: "what do you want to be added in the list"
+
+    },
+
+    {
+        name: "addanother",
+        type: "confirm",
+        message: "Do you want to add something else",
+        default: "false"
+    }
+])
+
+todos.push(task.todo)
+scenerio= task.addanother
+console.log(todos)
+}
